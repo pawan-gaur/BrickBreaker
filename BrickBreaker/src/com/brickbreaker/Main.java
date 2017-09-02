ef96c23fe6286c92a127f868172db829b22d0942
PGaur <PGaur@PGaur-Laptop>
2017-09-02 20:11:59 +0530

    Window frame is created

--- BrickBreaker/src/com/brickbreaker/Main.java
+++ BrickBreaker/src/com/brickbreaker/Main.java
@@ -6,6 +6,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		JFrame obj = new JFrame();
+		obj.setBounds(10, 10, 700, 600);
+		obj.setTitle("Breakout Ball");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
