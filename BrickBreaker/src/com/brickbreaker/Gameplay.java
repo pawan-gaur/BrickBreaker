6b9ca9a076a40627ce4ba050e7aa85f8d616d731
PGaur <PGaur@PGaur-Laptop>
2017-09-02 21:06:02 +0530

    Added ball and paddle

---
+++ BrickBreaker/src/com/brickbreaker/Gameplay.java
@@ -0,0 +1,125 @@
+package com.brickbreaker;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener {
+	
+	private boolean play = false;
+	private int score = 0;
+	private int totalBricks = 21;
+	
+	private Timer timer;
+	private int delay = 8;
+	
+	private int playerX = 310;
+	
+	private int ballposX = 120;
+	private int ballposY = 350;
+	private int ballXdir = -1;
+	private int ballYdir = -2;
+	
+	public 	Gameplay() {
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+		timer = new Timer(delay, this);
+		timer.start();
+	}
+	
+	public void paint(Graphics g){
+		//Background
+		g.setColor(Color.black);
+		g.fillRect(1, 1, 692, 592);
+		
+		//Borders
+		g.setColor(Color.yellow);
+		g.fillRect(0, 0, 3, 592);
+		g.fillRect(0, 0, 692, 3);
+		g.fillRect(691, 0, 3, 592);
+		
+		//Creating the Paddle
+		g.setColor(Color.green);
+		g.fillRect(playerX, 550, 100, 8);
+		
+		//Creating the Ball
+		g.setColor(Color.yellow);
+		g.fillOval(ballposX, ballposY, 20, 20);
+		
+		g.dispose();
+	}
+	
+	public void moveRight(){
+		play = true;
+		playerX+=20;
+	}
+	
+	public void moveLeft(){
+		play = true;
+		playerX-=20;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		timer.start();
+		if(play){
+			if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))){
+				ballYdir = -ballYdir;
+			}
+			
+			ballposX += ballXdir;
+			ballposY += ballYdir;
+			//Left Border
+			if(ballposX < 0){
+				ballXdir = -ballXdir; 
+			}
+			//Top Border
+			if(ballposY < 0){
+				ballYdir = -ballYdir; 
+			}
+			//Right Border
+			if(ballposX > 670){
+				ballXdir = -ballXdir; 
+			}
+		}
+		repaint();
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+			if(playerX >= 600){
+				playerX = 600;
+			}else{
+				moveRight();
+			}
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_LEFT){
+			if(playerX < 10){
+				playerX = 10;
+			}else{
+				moveLeft();
+			}
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+	}
+
+}
