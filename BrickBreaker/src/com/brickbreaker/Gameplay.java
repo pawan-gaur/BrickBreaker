95e88e920a57f52e861e1282869606dc4ed91f61
PGaur <PGaur@PGaur-Laptop>
2017-09-03 20:42:20 +0530

    Development Complete. Final Commit

--- BrickBreaker/src/com/brickbreaker/Gameplay.java
+++ BrickBreaker/src/com/brickbreaker/Gameplay.java
@@ -1,7 +1,9 @@
 package com.brickbreaker;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,7 +29,10 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 	private int ballXdir = -1;
 	private int ballYdir = -2;
 	
+	private MapGenerator map;
+	
 	public 	Gameplay() {
+		map = new MapGenerator(3, 7);
 		addKeyListener(this);
 		setFocusable(true);
 		setFocusTraversalKeysEnabled(false);
@@ -40,11 +45,19 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 		g.setColor(Color.black);
 		g.fillRect(1, 1, 692, 592);
 		
+		//Drawing Map
+		map.draw((Graphics2D) g);
+		
 		//Borders
 		g.setColor(Color.yellow);
 		g.fillRect(0, 0, 3, 592);
 		g.fillRect(0, 0, 692, 3);
 		g.fillRect(691, 0, 3, 592);
+		
+		//Displaying Score
+		g.setColor(Color.white);
+		g.setFont(new Font("serif", Font.BOLD, 25));
+		g.drawString(""+score, 592, 30);
 		
 		//Creating the Paddle
 		g.setColor(Color.green);
@@ -53,6 +66,30 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 		//Creating the Ball
 		g.setColor(Color.yellow);
 		g.fillOval(ballposX, ballposY, 20, 20);
+		if(totalBricks <=0 ){
+			play = false;
+			ballXdir = 0;
+			ballYdir = 0;
+			g.setColor(Color.red);
+			g.setFont(new Font("serif", Font.BOLD, 30));
+			g.drawString("Stage Complete", 190, 300);
+			
+			g.setFont(new Font("serif", Font.BOLD, 20));
+			g.drawString("Press Enter to Restart", 230, 350);
+		}
+		
+		
+		if(ballposY > 570){
+			play = false;
+			ballXdir = 0;
+			ballYdir = 0;
+			g.setColor(Color.red);
+			g.setFont(new Font("serif", Font.BOLD, 30));
+			g.drawString("Game Over! Scores : "+score, 190, 300);
+			
+			g.setFont(new Font("serif", Font.BOLD, 20));
+			g.drawString("Press Enter to Restart", 230, 350);
+		}
 		
 		g.dispose();
 	}
@@ -73,6 +110,35 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 		if(play){
 			if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))){
 				ballYdir = -ballYdir;
+			}
+			
+			A: for(int i = 0; i < map.map.length; i++){
+				for(int j = 0; j < map.map[0].length; j++){
+					if(map.map[i][j] > 0){
+						int brickX = j*map.brickWidth + 80;
+						int brickY = i*map.brickHeight + 50;
+						int brickWidth = map.brickWidth;
+						int brickHeight = map.brickHeight;
+						
+						Rectangle rect = new Rectangle(brickX, brickY, brickWidth, brickHeight);
+						Rectangle ballRect = new Rectangle(ballposX, ballposY, 20, 20);
+						Rectangle brickRect = rect;
+						
+						if(ballRect.intersects(brickRect)){
+							map.setBrickValue(0, i, j);
+							totalBricks--;
+							score += 5;
+							
+							if(ballposX	+ 19 <= brickRect.x || ballposX + 1 >= brickRect.x + brickRect.width){
+								ballXdir = -ballXdir;
+							}else{
+								ballYdir = -ballYdir;
+							}
+							
+							break A;
+						}
+					}
+				}
 			}
 			
 			ballposX += ballXdir;
@@ -113,6 +179,22 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 				playerX = 10;
 			}else{
 				moveLeft();
+			}
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_ENTER){
+			if(!play){
+				play = true;
+				ballposX = 120;
+				ballposY = 350;
+				ballXdir = -1;
+				ballYdir = -2;
+				playerX = 310;
+				score = 0;
+				totalBricks = 21;
+				map = new MapGenerator(3, 7);
+				
+				repaint(); 
 			}
 		}
 	}
